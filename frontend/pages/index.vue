6fb5559a8fe611564060d07f08bb216bb341f9d7
KvJellyBean <kevinnatan91@gmail.com>
2026-01-11 19:00:23 +0700

    refactor: remove auth check from index page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -9,16 +9,3 @@
     </div>
   </div>
 </template>
-
-<script setup lang="ts">
-const authStore = useAuthStore()
-
-onMounted(() => {
-  if (process.client) {
-    authStore.loadFromStorage()
-    if (authStore.isAuthenticated) {
-      navigateTo('/dashboard')
-    }
-  }
-})
-</script>
